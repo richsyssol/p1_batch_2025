ab2801a365a3613984f557fcd512d287368a5272
YogeshSonawane <yogeshbsonawane786@gmail.com>
2025-06-19 11:39:24 +0530

    adasf

--- JavaScript/11_Primitive_Reference/clone_array.js
+++ JavaScript/11_Primitive_Reference/clone_array.js
@@ -1,27 +1,4 @@
-
-
-// ############### Clone Array In JavaScript ####################
-// how to clone array
-// how to concatenate two arrays
-
-// 1. Slice Method
-// 2. Concat
-// 3. Spread Operator
-
-// let array_1=["item1","item2"];
-// let array_2 = array_1;
-// let array_2=["item1","item2"];
-// breifJs - intro and basic till strings and variable
-// string.md - string and strings methods
-// array.md - array and array methods
-
-array_1.push("item3");
-
-console.log(array_1==array_2);
-console.log(array_1);
-console.log(array_2);
-
-
+    
 
 /*
 // cloning with slice
---
+++ JavaScript/14_Imp_Array_methods/methods.js
@@ -0,0 +1,30 @@
+    const numbers = [4,2,5,8];
+    numbers.forEach((value,index )=>{
+    console.log(value);
+    console.log(index);
+    })
+    numbers.forEach(function numloop(){
+        
+})
+const number = [4,3,5,6,5];
+
+
+const sqr = function (number){
+    return number * number;
+}
+
+const sqrnumber = number.map(sqr);
+console.log(sqrnumber);
+
+
+
+// we can also write it as
+/*
+const sqrnumber = numbers.map((number)=>{
+     
+    return number*number;
+
+})
+console.log(sqrnumber);
+*/
+
