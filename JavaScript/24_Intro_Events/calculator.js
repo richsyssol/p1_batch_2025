8b5d2080bb3c2d06f31baf9797a24d9955e71684
YogeshSonawane <yogeshbsonawane786@gmail.com>
2025-07-01 13:04:02 +0530

    d

---
+++ JavaScript/24_Intro_Events/calculator.js
@@ -0,0 +1,23 @@
+let valueA=0;
+let valueB=0;
+const inputboxA=document.getElementById("valueA");
+const inputboxB=document.querySelector("#valueB");
+console.log(inputboxA);
+console.log(inputboxB);
+inputboxA.addEventListener("keyup",(event)=>{
+    valueA=Number(event.target.value);
+    console.log(event.target.value)})
+inputboxB.addEventListener("keyup",(event)=>{
+    valueB=Number(event.target.value);
+    console.log(event.target.value)})
+const button=document.getElementById("add");
+
+button.addEventListener("click",()=>{
+    alert(valueA+valueB)
+})
+
+
+
+
+
+
--- JavaScript/25_EventFlow/event_bubbling.js
+++ JavaScript/25_EventFlow/event_bubbling.js
@@ -4,7 +4,32 @@
 
 
 const grandparent = document.querySelector(".grandparent");
-// 
+// const parent = document.querySelector(".parent");
+// const child = document.querySelector(".child");
+
+// // addEventListener (event,function,capture="true")
+// // Capturing Events
+
+
+// child.addEventListener("click",()=>{
+//     console.log("Capture !!!!!!  Child !!");
+// },false);
+
+
+// parent.addEventListener("click",()=>{
+//     console.log("Capture !!!!!!  parent !!");
+// },false);
+
+
+// grandparent.addEventListener("click",()=>{
+//     console.log("Capture !!!!!!  grandparent !!");
+// },false);
+
+
+// document.body.addEventListener("click",()=>{
+//     console.log("Capture !!!!!!  body !!");
+// },false);
+
 
 // addEventListener (event,function,capture="false")
 //  Event Bubbling / Event Propogation.
